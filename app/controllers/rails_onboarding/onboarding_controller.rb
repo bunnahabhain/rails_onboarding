0c03c6c1302104dc807c5bdfc54e1c215ddc7a75
David Lewis <david@davidsfolly.com>
2025-08-25 11:47:56 -0400

    Fix to skip step

--- app/controllers/rails_onboarding/onboarding_controller.rb
+++ app/controllers/rails_onboarding/onboarding_controller.rb
@@ -58,8 +58,12 @@ module RailsOnboarding
 
     def skip
       if @current_step && @current_step[:skippable]
-        current_user.complete_onboarding_step!(@current_step[:name])
-        redirect_to onboarding_path
+        current_user.skip_onboarding_step!(@current_step[:name])
+        if current_user.onboarding_completed?
+          redirect_to_after_completion
+        else
+          redirect_to onboarding_path
+        end
       else
         current_user.skip_onboarding!
         redirect_to_after_skip
